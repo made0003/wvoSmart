772a0e12601d522cfbf0a2f68fa166dad3c592ae
Julian Steenbakker <juliansteenbakker@outlook.com>
2019-10-24 15:26:47 +0200

    Added viberation mode and dossier

--- DesignSprintMockUp/app/src/main/java/com/wvoz/designsprint/DossierActivity.kt
+++ DesignSprintMockUp/app/src/main/java/com/wvoz/designsprint/DossierActivity.kt
@@ -1,10 +1,25 @@
 package com.wvoz.designsprint
 
+import android.content.Intent
+import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
+import androidx.databinding.DataBindingUtil
+import com.wvoz.designsprint.databinding.ActivityDossierBinding
 
 class DossierActivity : WearableActivity() {
+    private var mBinding: ActivityDossierBinding? = null
 
-    fun onCreate() {
-        super.onCreate(sav)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_dossier)
+
+        mBinding?.dossier?.setOnClickListener {
+            //            val i = Intent((), AgendaActivity::class.java)
+    //            startActivity(i)
+        }
+
+
+        // Enables Always-on
+        setAmbientEnabled()
     }
 }
