cdb0f61c87c5d88386d3498d9a8b8695279839e0
Julian Steenbakker <juliansteenbakker@outlook.com>
2019-10-24 21:16:44 +0200

    Added text to speech

--- DesignSprintMockUp/app/src/main/java/com/wvoz/designsprint/DossierActivity.kt
+++ DesignSprintMockUp/app/src/main/java/com/wvoz/designsprint/DossierActivity.kt
@@ -2,6 +2,7 @@ package com.wvoz.designsprint
 
 import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.support.wearable.activity.WearableActivity
 import androidx.databinding.DataBindingUtil
 import com.wvoz.designsprint.databinding.ActivityDossierBinding
@@ -16,17 +17,46 @@ class DossierActivity : WearableActivity() {
 
 
         mBinding?.dossier?.setOnClickListener {
-            if (mBinding?.recording == true) {
-                val i = Intent(this, LogConfirmActivity::class.java)
-                startActivity(i)
-                finish()
-            } else {
-                mBinding?.recording = true
-            }
+            displaySpeechRecognizer()
         }
 
 
         // Enables Always-on
         setAmbientEnabled()
+    }
+
+    private val SPEECH_REQUEST_CODE = 0
+
+
+    // Create an intent that can start the Speech Recognizer activity
+    private fun displaySpeechRecognizer() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+        )
+        // Start the activity, the intent will be populated with the speech text
+        startActivityForResult(intent, SPEECH_REQUEST_CODE)
+    }
+
+    // This callback is invoked when the Speech Recognizer returns.
+    // This is where you process the intent and extract the speech text from the intent.
+    override fun onActivityResult(
+        requestCode: Int, resultCode: Int,
+        data: Intent
+    ) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            val results = data.getStringArrayListExtra(
+                RecognizerIntent.EXTRA_RESULTS
+            )
+            val spokenText = results[0]
+            val i = Intent(this, LogConfirmActivity::class.java)
+            i.putExtra("text", spokenText)
+            startActivity(i)
+            finish()
+
+            // Do something with spokenText
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
