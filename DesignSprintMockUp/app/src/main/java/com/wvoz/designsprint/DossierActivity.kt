6d95653220656b79d4a366ccd0cfff3db06ef697
Julian Steenbakker <juliansteenbakker@outlook.com>
2019-10-24 15:09:26 +0200

    added icon

---
+++ DesignSprintMockUp/app/src/main/java/com/wvoz/designsprint/DossierActivity.kt
@@ -0,0 +1,10 @@
+package com.wvoz.designsprint
+
+import android.support.wearable.activity.WearableActivity
+
+class DossierActivity : WearableActivity() {
+
+    fun onCreate() {
+        super.onCreate(sav)
+    }
+}
---
+++ DesignSprintMockUp/app/src/main/java/com/wvoz/designsprint/Utils.kt
@@ -0,0 +1,2 @@
+package com.wvoz.designsprint
+
